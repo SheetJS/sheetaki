8444c7ab44c7ee98905fe3eaf0e41f9fc91687da
Duncan Phelps <phelps.duncan@gmail.com>
2021-05-25 17:58:01 -0500

    changed location of static files and port

--- api/server.js
+++ api/server.js
@@ -5,12 +5,14 @@ const upload = require('./upload/index');
 const save = require('./save/index');
 const file = require('./file/index');
 const bodyParser = require('body-parser');
-const port = 5000;
+const port = 50001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+//serve static files from build folder instead of public folder
+app.use('/', express.static('build'));
 
 app.use('/api/data', data);
 app.use('/api/upload', upload);
